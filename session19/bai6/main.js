bad5afee6cef15f6ef6759758fa13595d9e4d699
truongcpngquy <congquy010206@gmail.com>
2025-03-20 23:10:47 +0700

    session19

--- session19/bai6/main.js
+++ session19/bai6/main.js
@@ -5,10 +5,14 @@ let delta = (b * b) - 4 * a * c;
 let sqrtDelta = Math.sqrt(delta);
 let x1 = (-b + sqrtDelta) / (2 * a);
 let x2 = (-b - sqrtDelta) / (2 * a);
-if(delta < 0){
-    document.writeln("Phương trình vô nghiệm");
-} else if(delta === 0){
-    document.writeln("Phương trình có nghiệm kép: x1 = x2 = "+ x1)
-} else if(delta > 0){
-    document.writeln("Phương trình có hai nghiệm phân biệt x1 = "+ x1 +"và x2 ="+ x2);
+if(a === 0){
+    document.writeln("Không phải phương trình bậc 2");
+}else{
+    if(delta < 0){
+        document.writeln("Phương trình vô nghiệm");
+    } else if(delta === 0){
+        document.writeln("Phương trình có nghiệm kép: x1 = x2 = "+ x1);
+    } else if(delta > 0){
+        document.writeln("Phương trình có hai nghiệm phân biệt x1 = "+ x1 +" và x2 = "+ x2);
+    }
 }
